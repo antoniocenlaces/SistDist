36337bcbb57834077bd2b16820f4c34e1aafec8d
antoniocenlaces <antoniocenlaces@gmail.com>
2025-09-21 17:59:26 +0000

    Algunos errores en barrier.go corregidos

--- practica1/CodigoEjemploEsqueletoP1/barrier/barrier.go
+++ practica1/CodigoEjemploEsqueletoP1/barrier/barrier.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -32,7 +33,7 @@ func readEndpoints(filename string) ([]string, error) {
 }
 
 func handleConnection(conn net.Conn, barrierChan chan<- bool,
-                            received *map[string]bool, mu *sync.Mutex, n int) {
+	received *map[string]bool, mu *sync.Mutex, n int) {
 	defer conn.Close()
 	buf := make([]byte, 1024)
 	_, err := conn.Read(buf)
@@ -52,25 +53,22 @@ func handleConnection(conn net.Conn, barrierChan chan<- bool,
 
 // Get enpoints (IP adresse:port for each distributed process)
 func getEndpoints() ([]string, int, error) {
-    endpointsFile := os.Args[1]
-    var endpoints []string  // Por qué esta declaración ?
+	endpointsFile := os.Args[1]
+	var endpoints []string // Por qué esta declaración ?
 	lineNumber, err := strconv.Atoi(os.Args[2])
 	if err != nil || lineNumber < 1 {
 		fmt.Println("Invalid line number")
 	} else if endpoints, err = readEndpoints(endpointsFile); err != nil {
-		    fmt.Println("Error reading endpoints:", err)
-	    } else if lineNumber > len(endpoints) {
-		        fmt.Printf("Line number %d out of range\n", lineNumber)
-		        err = errors.New("Line number out of range")
-	    }
-    }
-
-    return endpoints,lineNumber, err
+		fmt.Println("Error reading endpoints:", err)
+	} else if lineNumber > len(endpoints) {
+		fmt.Printf("Line number %d out of range\n", lineNumber)
+		err = errors.New("Line number out of range")
+	}
+	return endpoints, lineNumber, err
 }
 
 func acceptAndHandleConnections(listener net.Listener, quitChannel chan bool,
-            barrierChan chan bool, receivedMap *map[string]bool, mu *sync.Mutex)
- {
+	barrierChan chan bool, receivedMap *map[string]bool, mu *sync.Mutex) {
 	for {
 		select {
 		case <-quitChannel:
@@ -82,12 +80,12 @@ func acceptAndHandleConnections(listener net.Listener, quitChannel chan bool,
 				fmt.Println("Error accepting connection:", err)
 				continue
 			}
-			go handleConnection(conn, barrierChan, &receivedMap, &mu, n)
+			go handleConnection(conn, barrierChan, receivedMap, mu, n)
 		}
 	}
 }
 
-func notifyOtherDistributedProcesses(endPoints [] string, lineNumber int) {
+func notifyOtherDistributedProcesses(endPoints []string, lineNumber int) {
 	for i, ep := range endpoints {
 		if i+1 != lineNumber {
 			go func(ep string) {
@@ -113,28 +111,29 @@ func notifyOtherDistributedProcesses(endPoints [] string, lineNumber int) {
 }
 
 func main() {
-    var listener net.Listener
+	var listener net.Listener
 
 	if len(os.Args) != 3 {
 		fmt.Println("Usage: go run main.go <endpoints_file> <line_number>")
 	} else if endPoints, lineNumber, err := getEndpoints(); err != nil {
-        // Get the endpoint for current process
-        localEndpoint := endPoints[lineNumber-1]
-	    if listener, err = net.Listen("tcp", localEndpoint); err != nil {
-		    fmt.Println("Error creating listener:", err)
+		// Get the endpoint for current process
+		localEndpoint := endPoints[lineNumber-1]
+		if listener, err = net.Listen("tcp", localEndpoint); err != nil {
+			fmt.Println("Error creating listener:", err)
 		} else {
-            fmt.Println("Listening on", localEndpoint)
-
-    // Barrier synchronization
+			fmt.Println("Listening on", localEndpoint)
+		}
+	}
+	// Barrier synchronization
 	var mu sync.Mutex
 	quitChannel := make(chan bool)
 	receivedMap := make(map[string]bool)
 	barrierChan := make(chan bool)
 
-    go acceptAndHandleConnections(listener, quitChannel, barrierChan,
-                                                             &receivedMap, &mu)
+	go acceptAndHandleConnections(listener, quitChannel, barrierChan,
+		&receivedMap, &mu)
 
-    notifyOtherDistributedProcesses(endPoints, lineNumber)
+	notifyOtherDistributedProcesses(endPoints, lineNumber)
 
 	fmt.Println("Waiting for all the processes to reach the barrier")
 
