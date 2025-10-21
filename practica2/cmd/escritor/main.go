ac8a6fb88277155ed8fd6f628cd6b7a66c60a53b
root <root@PC-RUBEN.localdomain>
2025-10-21 20:15:25 +0200

    Codigo del lector y el escritor esta en proceso -Ruben-

--- practica2/cmd/escritor/main.go
+++ practica2/cmd/escritor/main.go
@@ -1,10 +1,48 @@
 package main
 
 import (
-
+	"fmt"
+	"log"
+	"os"
+	"practica2/ra"
+	"strconv"
 )
 
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func writeFile() {
+
+}
 
 func main() {
+	args := os.Args
+
+	if len(args) != 3 {
+		log.Println("usage: main.go peers me path")
+		os.Exit(1)
+	}
+
+	peersFile := args[1]
+	me, err := strconv.Atoi(args[2])
+	checkError(err)
+	path := args[3]
+
+	distributedMutex := ra.New(me, peersFile, false)
+	for {
+		distributedMutex.PreProtocol()
+
+		data, err := os.ReadFile(path)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s", err.Error())
+		} else {
+			log.Println("File content: ", data)
+		}
+		distributedMutex.PostProtocol()
+	}
 
 }
