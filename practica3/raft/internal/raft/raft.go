48ed7ddd31fe4751a5b504db29d1c4b61b1dc8a9
antoniocenlaces <antoniocenlaces@gmail.com>
2025-11-03 20:55:48 +0000

    inicio estructura NodoRaft

--- practica3/raft/internal/raft/raft.go
+++ practica3/raft/internal/raft/raft.go
@@ -22,20 +22,20 @@ package raft
 
 // type AplicaOperacion
 
-
 import (
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+
 	//"crypto/rand"
 	"sync"
 	"time"
+
 	//"net/rpc"
 
 	"raft/internal/comun/rpctimeout"
 )
-
 
 const (
 	// Constante para fijar valor entero no inicializado
@@ -53,39 +53,57 @@ const (
 )
 
 type TipoOperacion struct {
-	Operacion string  // La operaciones posibles son "leer" y "escribir"
-	Clave string
-	Valor string    // en el caso de la lectura Valor = ""
+	Operacion string // La operaciones posibles son "leer" y "escribir"
+	Clave     string
+	Valor     string // en el caso de la lectura Valor = ""
 }
-
 
 // A medida que el nodo Raft conoce las operaciones de las  entradas de registro
 // comprometidas, envía un AplicaOperacion, con cada una de ellas, al canal
-// "canalAplicar" (funcion NuevoNodo) de la maquina de estados 
+// "canalAplicar" (funcion NuevoNodo) de la maquina de estados
 type AplicaOperacion struct {
-	Indice int  // en la entrada de registro
+	Indice    int // en la entrada de registro
 	Operacion TipoOperacion
 }
 
 // Tipo de dato Go que representa un solo nodo (réplica) de raft
-//
 type NodoRaft struct {
-	Mux   sync.Mutex       // Mutex para proteger acceso a estado compartido
+	Mux sync.Mutex // Mutex para proteger acceso a estado compartido
 
 	// Host:Port de todos los nodos (réplicas) Raft, en mismo orden
-	Nodos []rpctimeout.HostPort
-	Yo    int           // indice de este nodos en campo array "nodos"
-	IdLider int
-	// Utilización opcional de este logger para depuración
-	// Cada nodo Raft tiene su propio registro de trazas (logs)
-	Logger *log.Logger
+	Nodos   []rpctimeout.HostPort
+	Yo      int         // indice de este nodos en campo array "nodos"
+	IdLider int         // índice en Nodos del líder
+	Logger  *log.Logger // para facilitar depuración
 
-	// Vuestros datos aqui.
-	
-	// mirar figura 2 para descripción del estado que debe mantenre un nodo Raft
+	//
+
+	// Estado persistente en todos los nodos
+	currentTerm int               // último mandato visto por el nodo. 0 al inicializar.
+	votedFor    int               // candidato que recibió mi voto en mandato actual
+	logEntries  []AplicaOperacion // registro de entradas en máquina estados
+
+	// Estado volatil
+	commitIndex int // índice de la mayor entrada en logEntries que ha de ser
+	// aplicada. Inicializa a 0.
+	lastApplied int // índice de la mayor entrada en logEntries que ha sido
+	// aplicada.
+
+	// Solo en líder
+	nextIndex map[int]int // para cada servidor, índice de la siguiente
+	// entrada a ser enviada a ese servidor (inicializa a último índice
+	// de logEntries del líder + 1)
+	matchIndex map[int]int // para cada servidor, almacena el índice
+	// más alto conocido a ser replicado en ese servidor
+
+	// Estado temporal (no persistente)
+	role          string      // "Follower", "Candidate", "Leader"
+	votesReceived int         // votos durante elección
+	electionReset time.Time   // última vez que recibió mensaje válido
+	timer         *time.Timer // para gestionar timeout de elecciones
+	// Canal para aplicar operaciones comprometidas a la máquina de estados
+	canalAplicarOperacion chan AplicaOperacion
 }
-
-
 
 // Creacion de un nuevo nodo de eleccion
 //
@@ -101,8 +119,8 @@ type NodoRaft struct {
 //
 // NuevoNodo() debe devolver resultado rápido, por lo que se deberían
 // poner en marcha Gorutinas para trabajos de larga duracion
-func NuevoNodo(nodos []rpctimeout.HostPort, yo int, 
-						canalAplicarOperacion chan AplicaOperacion) *NodoRaft {
+func NuevoNodo(nodos []rpctimeout.HostPort, yo int,
+	canalAplicarOperacion chan AplicaOperacion) *NodoRaft {
 	nr := &NodoRaft{}
 	nr.Nodos = nodos
 	nr.Yo = yo
@@ -111,24 +129,24 @@ func NuevoNodo(nodos []rpctimeout.HostPort, yo int,
 	if kEnableDebugLogs {
 		nombreNodo := nodos[yo].Host() + "_" + nodos[yo].Port()
 		logPrefix := fmt.Sprintf("%s", nombreNodo)
-		
+
 		fmt.Println("LogPrefix: ", logPrefix)
 
 		if kLogToStdout {
-			nr.Logger = log.New(os.Stdout, nombreNodo + " -->> ",
-								log.Lmicroseconds|log.Lshortfile)
+			nr.Logger = log.New(os.Stdout, nombreNodo+" -->> ",
+				log.Lmicroseconds|log.Lshortfile)
 		} else {
 			err := os.MkdirAll(kLogOutputDir, os.ModePerm)
 			if err != nil {
 				panic(err.Error())
 			}
 			logOutputFile, err := os.OpenFile(fmt.Sprintf("%s/%s.txt",
-			  kLogOutputDir, logPrefix), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+				kLogOutputDir, logPrefix), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
 			if err != nil {
 				panic(err.Error())
 			}
-			nr.Logger = log.New(logOutputFile, 
-						   logPrefix + " -> ", log.Lmicroseconds|log.Lshortfile)
+			nr.Logger = log.New(logOutputFile,
+				logPrefix+" -> ", log.Lmicroseconds|log.Lshortfile)
 		}
 		nr.Logger.Println("logger initialized")
 	} else {
@@ -136,24 +154,21 @@ func NuevoNodo(nodos []rpctimeout.HostPort, yo int,
 	}
 
 	// Añadir codigo de inicialización
-	
 
 	return nr
 }
-
 
 // Metodo Para() utilizado cuando no se necesita mas al nodo
 //
 // Quizas interesante desactivar la salida de depuracion
 // de este nodo
-//
 func (nr *NodoRaft) para() {
-	go func() {time.Sleep(5 * time.Millisecond); os.Exit(0) } ()
+	go func() { time.Sleep(5 * time.Millisecond); os.Exit(0) }()
 }
 
 // Devuelve "yo", mandato en curso y si este nodo cree ser lider
 //
-// Primer valor devuelto es el indice de este  nodo Raft el el conjunto de nodos 
+// Primer valor devuelto es el indice de este  nodo Raft el el conjunto de nodos
 // la operacion si consigue comprometerse.
 // El segundo valor es el mandato en curso
 // El tercer valor es true si el nodo cree ser el lider
@@ -162,11 +177,9 @@ func (nr *NodoRaft) obtenerEstado() (int, int, bool, int) {
 	var yo int = nr.Yo
 	var mandato int
 	var esLider bool
-	var idLider int =nr.IdLider
-	
+	var idLider int = nr.IdLider
 
 	// Vuestro codigo aqui
-	
 
 	return yo, mandato, esLider, idLider
 }
@@ -178,30 +191,27 @@ func (nr *NodoRaft) obtenerEstado() (int, int, bool, int) {
 // Si el nodo no es el lider, devolver falso
 // Sino, comenzar la operacion de consenso sobre la operacion y devolver en
 // cuanto se consiga
-// 
+//
 // No hay garantia que esta operacion consiga comprometerse en una entrada de
 // de registro, dado que el lider puede fallar y la entrada ser reemplazada
 // en el futuro.
-// Primer valor devuelto es el indice del registro donde se va a colocar 
+// Primer valor devuelto es el indice del registro donde se va a colocar
 // la operacion si consigue comprometerse.
 // El segundo valor es el mandato en curso
 // El tercer valor es true si el nodo cree ser el lider
 // Cuarto valor es el lider, es el indice del líder si no es él
 func (nr *NodoRaft) someterOperacion(operacion TipoOperacion) (int, int,
-															bool, int, string) {
+	bool, int, string) {
 	indice := -1
 	mandato := -1
 	EsLider := false
 	idLider := -1
 	valorADevolver := ""
-	
 
 	// Vuestro codigo aqui
-	
 
 	return indice, mandato, EsLider, idLider, valorADevolver
 }
-
 
 // -----------------------------------------------------------------------
 // LLAMADAS RPC al API
@@ -209,24 +219,24 @@ func (nr *NodoRaft) someterOperacion(operacion TipoOperacion) (int, int,
 // Si no tenemos argumentos o respuesta estructura vacia (tamaño cero)
 type Vacio struct{}
 
-func (nr * NodoRaft) ParaNodo(args Vacio, reply *Vacio) error {
+func (nr *NodoRaft) ParaNodo(args Vacio, reply *Vacio) error {
 	defer nr.para()
 	return nil
 }
 
 type EstadoParcial struct {
-	Mandato	int
+	Mandato int
 	EsLider bool
-	IdLider	int
+	IdLider int
 }
 
 type EstadoRemoto struct {
-	IdNodo	int
+	IdNodo int
 	EstadoParcial
 }
 
 func (nr *NodoRaft) ObtenerEstadoNodo(args Vacio, reply *EstadoRemoto) error {
-	reply.IdNodo,reply.Mandato,reply.EsLider,reply.IdLider = nr.obtenerEstado()
+	reply.IdNodo, reply.Mandato, reply.EsLider, reply.IdLider = nr.obtenerEstado()
 	return nil
 }
 
@@ -237,9 +247,9 @@ type ResultadoRemoto struct {
 }
 
 func (nr *NodoRaft) SometerOperacionRaft(operacion TipoOperacion,
-												reply *ResultadoRemoto) error {
-	reply.IndiceRegistro,reply.Mandato, reply.EsLider,
-			reply.IdLider,reply.ValorADevolver = nr.someterOperacion(operacion)
+	reply *ResultadoRemoto) error {
+	reply.IndiceRegistro, reply.Mandato, reply.EsLider,
+		reply.IdLider, reply.ValorADevolver = nr.someterOperacion(operacion)
 	return nil
 }
 
@@ -251,7 +261,6 @@ func (nr *NodoRaft) SometerOperacionRaft(operacion TipoOperacion,
 // Recordar
 // -----------
 // Nombres de campos deben comenzar con letra mayuscula !
-//
 type ArgsPeticionVoto struct {
 	// Vuestros datos aqui
 }
@@ -261,22 +270,17 @@ type ArgsPeticionVoto struct {
 // Recordar
 // -----------
 // Nombres de campos deben comenzar con letra mayuscula !
-//
-//
 type RespuestaPeticionVoto struct {
 	// Vuestros datos aqui
 }
 
-
 // Metodo para RPC PedirVoto
-//
 func (nr *NodoRaft) PedirVoto(peticion *ArgsPeticionVoto,
-										reply *RespuestaPeticionVoto) error {
+	reply *RespuestaPeticionVoto) error {
 	// Vuestro codigo aqui
 
-	return nil	
+	return nil
 }
-
 
 type ArgAppendEntries struct {
 	// Vuestros datos aqui
@@ -286,15 +290,13 @@ type Results struct {
 	// Vuestros datos aqui
 }
 
-
 // Metodo de tratamiento de llamadas RPC AppendEntries
 func (nr *NodoRaft) AppendEntries(args *ArgAppendEntries,
-													  results *Results) error {
+	results *Results) error {
 	// Completar....
 
 	return nil
 }
-
 
 // ----- Metodos/Funciones a utilizar como clientes
 //
@@ -326,12 +328,10 @@ func (nr *NodoRaft) AppendEntries(args *ArgAppendEntries,
 // pasadas como parametros en las llamadas RPC es una mayuscula,
 // Y que la estructura de recuperacion de resultado sea un puntero a estructura
 // y no la estructura misma.
-//
 func (nr *NodoRaft) enviarPeticionVoto(nodo int, args *ArgsPeticionVoto,
-											reply *RespuestaPeticionVoto) bool {
-	
+	reply *RespuestaPeticionVoto) bool {
 
 	// Completar....
-	
+
 	return true
 }
