8f77f7ed53f3a5a9ebca427588681c332c318fe4
antoniocenlaces <antoniocenlaces@gmail.com>
2025-12-04 12:16:00 +0000

    practica 5 con main para kubectl

--- practica5/raft/cmd/srvraft/main.go
+++ practica5/raft/cmd/srvraft/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	//"errors"
 	"fmt"
+	"time"
+
 	//"log"
 	"net"
 	"net/rpc"
@@ -11,14 +13,16 @@ import (
 	"raft/internal/comun/rpctimeout"
 	"raft/internal/raft"
 	"strconv"
+	"strings"
 	//"time"
 )
 
 func main() {
-	// obtener entero de indice de este nodo
-	me, err := strconv.Atoi(os.Args[1])
-	check.CheckError(err, "Main, mal numero entero de indice de nodo:")
-
+	rawID := os.Args[1] // Ej: "raft-0"
+	idStr := strings.TrimPrefix(rawID, "raft-")
+	me, err := strconv.Atoi(idStr)
+	check.CheckError(err, "Main: fallo convirtiendo ID del nodo")
+	fmt.Printf("Replica %s iniciada como nodo %d\n", rawID, me)
 	var nodos []rpctimeout.HostPort
 	// Resto de argumento son los end points como strings
 	// De todas la replicas-> pasarlos a HostPort
@@ -30,10 +34,28 @@ func main() {
 	nr := raft.NuevoNodo(nodos, me, make(chan raft.AplicaOperacion, 1000))
 	rpc.Register(nr)
 
+	// fmt.Println("Métodos RPC registrados:")
+	// t := reflect.TypeOf(nr)
+	// for i := 0; i < t.NumMethod(); i++ {
+	// 	fmt.Println("-", t.Method(i).Name)
+	// }
+
 	fmt.Println("Replica escucha en :", me, " de ", os.Args[2:])
 
-	l, err := net.Listen("tcp", os.Args[2:][me])
-	check.CheckError(err, "Main listen error:")
+	puerto := ":29280"
+	var l net.Listener
+	for {
+		fmt.Println("Intentando abrir puerto", puerto)
+		var err error
+		l, err = net.Listen("tcp", puerto)
+		if err == nil {
+			fmt.Println("✔ Puerto disponible → servidor escuchando en", puerto)
+			break
+		}
+
+		fmt.Println("❌ Fallo en listen:", err)
+		time.Sleep(2 * time.Second)
+	}
 
 	rpc.Accept(l)
 }
